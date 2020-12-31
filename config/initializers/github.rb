acb8f3b0cd1b8acd3ce4c21e9e64b3dcac5a8031
Zach Stearns <zach.stearns92@gmail.com>
2020-12-31 00:17:08 -0700

    Rename file

--- config/initializers/github.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['f44e7d46afec15896c18'], ENV['01094476143a2604ea4404b3f247b079be271871'], scope: "user,repo"
-end
