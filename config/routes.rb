c8266e2cb80312023b710e22e22e2c526e342d9c
Zach Stearns <zach.stearns92@gmail.com>
2020-12-31 00:53:44 -0700

    Add dashboard index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   get '/auth/github', as: :github_login
   get '/auth/github/callback', to: 'sessions#create'
+
+  get '/dashboard', to: 'dashboard#index', as: :dashboard
 end
