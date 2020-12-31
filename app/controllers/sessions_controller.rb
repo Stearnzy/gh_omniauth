ccb07aef2b98279e720d388b7bbcced3ffc59627
Zach Stearns <zach.stearns92@gmail.com>
2020-12-31 00:54:29 -0700

    Create/find user, tie to session, redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
   def create
-    
+    user = User.find_or_create_by(uid: request_hash[:uid]) do |user|
+      user.username = request_hash[:info][:nickname]
+      user.token = request_hash[:credentials][:token]
+      user.save
+    end
+
+    session[:user_id] = user.id
+
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def request_hash
+    request.env['omniauth.auth']
   end
 end
