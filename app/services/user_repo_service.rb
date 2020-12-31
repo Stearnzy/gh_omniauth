0f17a58bed3ebe9ea9b7d798b87b9f2b3e8a2c19
Zach Stearns <zach.stearns92@gmail.com>
2020-12-31 12:49:10 -0700

    Pass current user through so conn can use token

--- app/services/user_repo_service.rb
+++ app/services/user_repo_service.rb
@@ -1,19 +1,19 @@
 class UserRepoService
-  def self.conn
+  def self.conn(current_user)
     Faraday.new(url: 'https://api.github.com', headers: { 'Authorization': "token #{current_user.token}" })
   end
 
-  def fetch_public_repos
-    response = conn.get("/user/repos?per_page=100&type=public")
+  def self.fetch_public_repos(current_user)
+    response = conn(current_user).get('/user/repos?per_page=100&type=public')
     parse_data(response)
   end
 
-  def fetch_private_repos
-    response = conn.get("/user/repos?per_page=100&type=private")
+  def self.fetch_private_repos(current_user)
+    response = conn(current_user).get('/user/repos?per_page=100&type=private')
     parse_data(response)
   end
 
-  def parse_data(response)
-    data = JSON.parse(response.body, symbolize_names: true)
+  def self.parse_data(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
