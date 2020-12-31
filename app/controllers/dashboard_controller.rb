515498afa89746642a143724da3c9a5048adf57a
Zach Stearns <zach.stearns92@gmail.com>
2020-12-31 12:47:16 -0700

    Add methods from facade to save values to instance variables

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    @public_repos = DashboardFacade.public_repos(current_user)
+    @private_repos = DashboardFacade.private_repos(current_user)
   end
 end
